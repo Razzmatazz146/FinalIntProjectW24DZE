2885d5c87e11693a726078bb33bb0e39df707c9c
Razzmatazz146 <denispletourneau@hotmail.com>
2024-05-14 14:41:07 -0400

    Base function to call python script and send image.

--- server/server.js
+++ server/server.js
@@ -16,27 +16,60 @@ app.get('/', function (req, res) {
 
 
 // Route for generating a graph
-app.post('/graphs/generate', (req, res) => {
-    // Extract parameters from request body
+app.post('/generate-graph', (req, res) => {
+    // Extract parameter strings from request body
+    // graphType : line, pie, bar
+    // countries : array of country names
+    // extraParams : array of extra parameters to pass to graphing library
     const { graphType, countries, extraParams } = req.body;
 
     // TODO: Use Python code to query the database and generate the graph
 
-    // TODO: Show image of graph before downloading
+    // Convert countries and extraParams to strings
+    const graphTypeStr = JSON.stringify(graphType)
+    const countriesStr = JSON.stringify(countries);
+    const extraParamsStr = JSON.stringify(extraParams);
+    console.log("Graph: ", graphTypeStr);
+    console.log("Countries: ", countriesStr);
+    console.log("Extras: ", extraParamsStr);
 
+    // Call Python script to generate graph
+    const python = spawn('python', ['./fake_graph.py', graphTypeStr, countriesStr, extraParamsStr]);
+
+    python.on('stderr', function (data) {
+        console.log('data: ', data);
+    })
+
+    python.on('stdout', function (data) {
+        console.log('data: ', data);
+    })
+
+
+    python.on('error', (err) => {
+        console.error(`Failed to start Python script: ${err.message}`);
+        return res.status(500).json({ message: 'Error generating graph' });
+    });
+
+    python.on('close', (code) => {
+        console.log('code: ', code);
+        if (code !== 0) {
+            console.error(`Python script exited with code ${code}`);
+            return res.status(500).json({ message: 'Error generating graph' });
+        }
+        const file_name = 'fake_graph.png';
+
+        const imagePath = path.join('images', file_name);
+
+        // Send the image file in the response
+        res.sendFile(imagePath, { root: __dirname })
+    });
+
+    // TODO: Clean-up after sending
 });
 
-// Route for getting graph samples
-app.get('/graphs', (req, res) => {
-    // TODO: Return a list of graphs with sample images
-});
-
-// Route for downloading graphs
-app.get('/graphs/download', (req, res) => {
-    // TODO: Return image of graph for user to download
-
-    // TODO: Manage and cleanup of the server’s file storage
-});
+app.get('/countries', (req, res) => {
+    // TODO: Return array of countries from DB
+})
 
 
 // Route for proof of concept
@@ -73,10 +106,10 @@ app.post('/test-graph', (req, res) => {
             return res.status(500).json({ message: 'Error generating graph' });
         }
 
-        const imagePath = path.join('images','fake_graph.png');
+        const imagePath = path.join('images', 'fake_graph.png');
 
         // Send the image file in the response
-        res.sendFile(imagePath, { root: __dirname})
+        res.sendFile(imagePath, { root: __dirname })
     });
 });
 
